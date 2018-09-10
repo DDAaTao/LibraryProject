dabf1b74282223dacf436b132a321e907acc4d5f
at <fanwentao123@qq.com>
2018-09-10 23:21:40 +0800

    'lombok'

--- src/main/java/com/library/LibraryProject/entity/User.java
+++ src/main/java/com/library/LibraryProject/entity/User.java
@@ -1,4 +1,16 @@
 package com.library.LibraryProject.entity;
 
+import lombok.Data;
+
+@Data
 public class User {
+    private Integer userId;
+    private String userName;
+    private String userNumber;
+    private String userPassword;
+    private String userSex;
+    private String userAcademy;
+    private String userProfession;
+    private String userViolation;
+    private String userStatus;
 }
