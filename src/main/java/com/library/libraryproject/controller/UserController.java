400ddf2d8c5acf2b337809f502a84f2516823ad8
macVan <fanwentao123@qq.com>
2019-02-16 16:40:56 +0800

    first

--- src/main/java/com/library/libraryproject/controller/UserController.java
+++ src/main/java/com/library/libraryproject/controller/UserController.java
@@ -11,7 +11,6 @@ import com.library.libraryproject.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
