1696bf8d24304c47b8977d2c409dd3c2d4a61d5c
macVan <fanwentao123@qq.com>
2019-03-17 00:21:52 +0800

    修复多线程scheduler问题

--- src/main/java/com/library/libraryproject/scheduler/SchedulerConfig.java
+++ src/main/java/com/library/libraryproject/scheduler/SchedulerConfig.java
@@ -2,6 +2,7 @@ package com.library.libraryproject.scheduler;
 
 import org.apache.commons.lang3.concurrent.BasicThreadFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.SchedulingConfigurer;
 import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 
@@ -18,6 +19,7 @@ import java.util.concurrent.ScheduledThreadPoolExecutor;
  * 采用实现接口的方式用以实现并发（并行）
  * 此处使用ScheduledThreadPool线程池进行控制
  * */
+@Configuration
 public class SchedulerConfig implements SchedulingConfigurer {
     @Override
     public void configureTasks(ScheduledTaskRegistrar scheduledTaskRegistrar) {
@@ -28,7 +30,7 @@ public class SchedulerConfig implements SchedulingConfigurer {
      * 此处shutdown曾报红是因为方法的修饰符为public，应为private
      * */
     @Bean(destroyMethod = "shutdown")
-    private Executor setTaskExecutor(){
+    public Executor setTaskExecutor(){
         // 此处的第一个参数为最大多少个线程
         ScheduledExecutorService scheduledExecutorService = new ScheduledThreadPoolExecutor(3
                 , new BasicThreadFactory.Builder().namingPattern("scheduler-pool-%d").daemon(true).build());
